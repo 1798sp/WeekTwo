44fcf620846351b9554dba4c55f88e1b1b840f48
ACCELYA\Shubham.Pawar <shubham.pawar@accelya.com>
2022-01-24 15:12:20 +0530

    green

--- WeekTwo/src/com/hello/Hello.java
+++ WeekTwo/src/com/hello/Hello.java
@@ -4,8 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< master
 		System.out.println("Helllo World");
 		System.out.println("git is saying hello");
+=======
+		System.out.println("Helllo");
+>>>>>>> e6639df Hello World and work created
 	}
 
 }
